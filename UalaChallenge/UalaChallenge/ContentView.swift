c1980842dfbbca40de49dd4d1dea058e4c369ecd
Liz Fabiola Isasi Cardozo <liz.isasi@icloud.com>
2025-01-27 14:25:09 -0300

    first commit

---
+++ UalaChallenge/UalaChallenge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UalaChallenge
+//
+//  Created by Liz Fabiola Isasi Cardozo on 27/01/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UalaChallenge/UalaChallenge/UalaChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  UalaChallengeApp.swift
+//  UalaChallenge
+//
+//  Created by Liz Fabiola Isasi Cardozo on 27/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct UalaChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
